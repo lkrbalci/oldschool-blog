ce0bf8b9f630f7272c02fa4ff6e8f4ef3b0d8261
lkrbalci <lkrbalci@gmail.com>
2024-05-26 00:04:27 +0300

    some navbar work for small screens

--- osb-frontend/src/components/navBar/navBar.tsx
+++ osb-frontend/src/components/navBar/navBar.tsx
@@ -14,22 +14,22 @@ const Navbar = () => {
     <header
       role="navigation"
       data-testid="navbar"
-      className="flex items-center justify-between px-4 text-xl m-1 border-2 border-secondary md:p-0 md:flex-col md:items-center md:justify-around md:w-1/6"
+      className="m-1 flex items-center border-2 border-secondary px-4 text-xl sm:justify-between md:w-1/6 md:flex-col md:items-center md:justify-around md:p-0"
     >
       {/* LOGO */}
-      <div className="h-8 w-8 relative hover:scale-150 hover:cursor-pointer md:h-24 md:w-24">
+      <div className="relative h-6 w-6 hover:scale-150 hover:cursor-pointer sm:h-8 sm:w-8 md:h-24 md:w-24">
         <Link href={"/"}>
           <Image src="/rootifera.png" alt="Rootifera Logo" fill />
         </Link>
       </div>
       {/* LINKS */}
-      <div className="flex items-center justify-evenly w-4/6 md:flex-col md:w-full md:text-2xl">
+      <div className="sm:4/6 flex w-5/6 items-center justify-center text-sm sm:justify-evenly sm:text-xl md:w-full md:flex-col md:text-2xl">
         {links.map((link, index) => (
           <NavLink link={link} index={index} key={link.title} />
         ))}
       </div>
       {/* SOCIAL */}
-      <div className="flex gap-2 items-center md:flex-col md:w-3/12">
+      <div className=" hidden items-center sm:flex sm:gap-2 md:w-3/12 md:flex-col">
         <Link href="/">
           <Image src="/github.png" alt="GitHub icon" width={24} height={24} />
         </Link>
